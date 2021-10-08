3fcb2c723542f1f9efdf0342862063a0ae7dc1a8
yann4bov <yann.4bov@gmail.com>
2021-10-08 08:51:58 -0300

    fixing onAuthRequired fuction

--- src/app/config/auth-config.ts
+++ src/app/config/auth-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oa24w16r1P7kHfrr5d7',
     issuer: 'https://dev-65537004.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://okta-authapp-angular.herokuapp.com/login/callback',
     scopes: ['email', 'profile','openid']
   }
 }
