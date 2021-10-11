77e1f6d8192aa1777fb1454b4f0b0696ad8234c8
Williams J B Silva <williams.cadastros@gmail.com>
2021-10-11 09:46:49 -0300

    add jwt decoder. welcome component changed

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OktaAuthStateService } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
+import jwt_decode from "jwt-decode";
 
 @Component({
   selector: 'app-welcome',
@@ -10,22 +11,25 @@ import { OktaAuth } from '@okta/okta-auth-js';
 export class WelcomeComponent implements OnInit {
 
   userName = '';
-  
+  token: any ;
+  tdecode: any ;
+
   constructor(
     public authStateService: OktaAuthStateService,
     private oktaAuth : OktaAuth
-  ) {
-  
-  }
+  ) { }
 
   async ngOnInit() {
-    
+
     const isAuthenticated = await this.oktaAuth.isAuthenticated();
-    
+
     if (isAuthenticated) {
-      
+
       const userClaims = await this.oktaAuth.getUser();
-      
+      this.token =  this.oktaAuth.getAccessToken();
+      console.log(this.token);
+
+      this.tdecode = jwt_decode(this.token);
       this.userName = userClaims.name as string;
     }
   }
